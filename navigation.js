59240081d7a8a4ed72e0c4e096fce252c3602ff1
Sophie <sophiecola@hotmail.com>
2020-03-30 14:32:44 +0200

    fonction noscroll

--- navigation.js
+++ navigation.js
@@ -5,4 +5,5 @@ toggleMenu.addEventListener('click', function() {
 	const open = JSON.parse(toggleMenu.getAttribute('aria-expanded'));
 	toggleMenu.setAttribute('aria-expanded', !open);
 	menu.hidden = !menu.hidden;
+	document.body.classList.toggle("noscroll");
 });
